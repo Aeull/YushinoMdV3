3132632153f3e606e5a3aa24ed84c342c3a7da4f
Aeull <110038479+Aeull@users.noreply.github.com>
2022-11-02 14:34:23 +0800

    Create tes.js

---
+++ plugins/tes.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+
+let info = `*ArullBotz Disini(｡>_<｡)*`
+
+
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+
+conn.sendButtonDoc(m.chat, info, wm, 'Hallo Bot', '.menu', m)
+await conn.sendMessage(m.chat, {
+      react: {
+       text: '👋',
+       key: m.key,
+       }})
+
+}
+
+handler.customPrefix = /^(tes|bot|test)$/i
+
+handler.command = new RegExp
+
+
+
+export default handler
