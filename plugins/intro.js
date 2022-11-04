e4b51a2750926d53fa79dd8d598763933967ef26
Aeull <110038479+Aeull@users.noreply.github.com>
2022-11-04 17:57:21 +0800

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -13,7 +13,7 @@ let krtu = `☰⃟⟣⟔ Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ ⟓⟢⃟
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendMessage(m.chat, krtu, m) // Tambah sendiri kalo mau
+conn.repy(m.chat, krtu, m) // Tambah sendiri kalo mau
 }
 handler.command = /^(intro)$/i
 
